90835a35b98b09664b4a10ed010e935225ed8eb9
linyuansup <linyuansup@gmail.com>
2025-09-21 15:10:06 +0800

    smtc

--- lib/ui/windows/page/home/init.dart
+++ lib/ui/windows/page/home/init.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:bilizen/inject/inject.dart';
 import 'package:bilizen/package/auto_update_manager.dart';
+import 'package:bilizen/package/playback_manager/playback_controller.dart';
 import 'package:bilizen/ui/windows/page/home/dialog/update/page.dart';
 import 'package:bilizen/util/toastification.dart';
 import 'package:fluent_ui/fluent_ui.dart';
@@ -36,25 +37,41 @@ void initUpdate(BuildContext context) {
 
 void initSmtc() {
   final smtc = getIt<SMTCWindows>();
-  smtc.buttonPressStream.listen((event) {
+  final playbackController = getIt<PlaybackController>();
+  smtc.buttonPressStream.listen((event) async {
     switch (event) {
       case PressedButton.play:
-        // Update playback status
-        smtc.setPlaybackStatus(PlaybackStatus.playing);
+        await playbackController.play();
         break;
       case PressedButton.pause:
-        smtc.setPlaybackStatus(PlaybackStatus.paused);
+        await playbackController.pause();
         break;
       case PressedButton.next:
+        await playbackController.next();
         break;
       case PressedButton.previous:
-        break;
-      case PressedButton.stop:
-        smtc.setPlaybackStatus(PlaybackStatus.stopped);
-        smtc.disableSmtc();
+        await playbackController.previous();
         break;
       default:
         break;
     }
   });
+  playbackController.currentPlaying.listen((media) async {
+    if (media == null) {
+      return;
+    } else {
+      await smtc.setPlaybackStatus(
+        media.isPlaying ? PlaybackStatus.playing : PlaybackStatus.paused,
+      );
+      final nickname = await (await media.item.video.uploader).nickName;
+      await smtc.setTitle(await media.item.video.title);
+      await smtc.setArtist(nickname);
+      await smtc.setThumbnail(await media.item.video.cover);
+      final totalDuration =
+          (await media.item.video.playlist)[media.item.pIndex].duration;
+      await smtc.setEndTime(Duration(seconds: totalDuration));
+      await smtc.setPosition(media.position);
+    }
+  });
+  smtc.setStartTime(Duration.zero);
 }
